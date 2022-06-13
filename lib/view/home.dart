433cfc2d1c9da5fce2df4538a5ea8a34d05986ef
renmurakami <autoeyy@gmail.com>
2022-06-13 11:24:29 +0900

    count state

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -4,12 +4,29 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 class HomeView extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final count = useState(0);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("hooks"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black87,
+        elevation: 1,
       ),
-      body: Center(
-        child: Text("hello hooks"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("hello hooks count: ${count.value.toString()}"),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              count.value++;
+            },
+            tooltip: "increment",
+            child: const Icon(Icons.add),
+          )
+        ],
       ),
     );
   }
